edfd9cd7f8249808ac01ae93cf22962f48fbb7d8
Williams Medina <williams.medinaa@gmail.com>
2018-06-13 12:09:29 -0600

    chore: add icon builder, project and archive file icons

--- tools/build.ts
+++ tools/build.ts
@@ -14,12 +14,28 @@ async function buildIcons (resolution: number) {
       'Shark.Folder': {
         "iconPath": `./images/${resolution}/Folder.svg`
       },
+      'Shark.Project': {
+        "iconPath": `./images/${resolution}/Project.svg`
+      },
       'Shark.File': {
         "iconPath": `./images/${resolution}/File.svg`
+      },
+      'Shark.Archive': {
+        "iconPath": `./images/${resolution}/Archive.svg`
       }
     },
-    'rootFolder': 'Shark.Folder',
-    'rootFolderExpanded': 'Shark.Folder',
+    'fileExtensions': {
+      'zip': 'Shark.Archive',
+      'rar': 'Shark.Archive',
+      'tar': 'Shark.Archive',
+      'sbx': 'Shark.Archive',
+      'gz': 'Shark.Archive',
+      'gzip': 'Shark.Archive',
+      '7z': 'Shark.Archive',
+      's7z': 'Shark.Archive'
+    },
+    'rootFolder': 'Shark.Project',
+    'rootFolderExpanded': 'Shark.Project',
     'folder': 'Shark.Folder',
     'folderExpanded': 'Shark.Folder',
     'file': 'Shark.File'
@@ -33,6 +49,14 @@ async function buildIcons (resolution: number) {
     path.resolve(__dirname, `${resolution}/Folder.svg`), 
     path.resolve(`fileicons/images/${resolution}/Folder.svg`)
   )
+  await copy(
+    path.resolve(__dirname, `${resolution}/Project.svg`), 
+    path.resolve(`fileicons/images/${resolution}/Project.svg`)
+  )
+  await copy(
+    path.resolve(__dirname, `${resolution}/Archive.svg`), 
+    path.resolve(`fileicons/images/${resolution}/Archive.svg`)
+  )
   return icons;
 }
 
@@ -42,9 +66,21 @@ async function copy (target: string, dest: string) {
     .pipe(fs.createWriteStream(dest));
 }
 
-async function readSVG (filePath: string) {
+async function readSVG (filePath: string, options?: any) {
   const fileSVGBuffer = await readFile(filePath);
-  const fileSVGContent = fileSVGBuffer.toString();
+  let fileSVGContent = fileSVGBuffer.toString();
+  if (options && options.idPrefix) {
+    const newIds = {};
+    fileSVGContent = fileSVGContent.replace(/id="([\w-.+]*)"/g, (match, id) => {
+      const newId = `${options.idPrefix}-${id}`;
+      newIds[id] = newId;
+      return `id="${newId}"`;
+    });
+    fileSVGContent = fileSVGContent.replace(/#([\w-.+]*)/g, (match, id) => {
+      const newId = newIds[id] || id;
+      return `#${newId}`;
+    });
+  }
   return await XMLParse(fileSVGContent);
 }
 
@@ -63,19 +99,21 @@ async function buildFileIcons (
   }
   const fileTokens = require('./file-tokens.json');
   const filePath = path.resolve(__dirname, `${resolution}/File.svg`);
-  const fileSVG = await readSVG(filePath);
   
   await fileTokens.reduce((promise, token) => {
     return promise.then(() => { 
       return new Promise(async (resolve) => {
-        let fileTokenSVG = Object.assign({}, fileSVG);
-        let fileIconPath = path.resolve(__dirname, `${resolution}/icons/${token.definition}.svg`);
-        let fileIconSVG = await readSVG(fileIconPath);
-        if (!fileTokenSVG.svg.defs) { 
-          fileTokenSVG.svg.defs = [{}];
+        const fileSVG = await readSVG(filePath);
+        const fileIconPath = path.resolve(__dirname, `${resolution}/icons/${token.definition}.svg`);
+        const fileIconSVG = await readSVG(fileIconPath, {
+          idPrefix: token.definition 
+        });
+        
+        if (!fileSVG.svg.defs) { 
+          fileSVG.svg.defs = [{}];
         }
         // Add Symbol Definition
-        Object.assign(fileTokenSVG.svg.defs[0], {
+        Object.assign(fileSVG.svg.defs[0], {
           symbol: [{
             $: {
               id: 'FileIcon'
@@ -84,10 +122,10 @@ async function buildFileIcons (
           }]
         });
         if (fileIconSVG.svg.defs && fileIconSVG.svg.defs[0]) {
-          merge(fileTokenSVG.svg.defs[0], fileIconSVG.svg.defs[0]);
+          merge(fileSVG.svg.defs[0], fileIconSVG.svg.defs[0]);
         }
         // Place Symbol
-        Object.assign(fileTokenSVG.svg, {
+        Object.assign(fileSVG.svg, {
           use: [{
             $: {
               'x': containerX,
@@ -99,7 +137,7 @@ async function buildFileIcons (
           }]
         })
         // Save File
-        const fileXMLContent = XMLBuilder.buildObject(fileTokenSVG);
+        const fileXMLContent = XMLBuilder.buildObject(fileSVG);
         const fileXMLPath = path.resolve(`fileicons/images/${resolution}/File-${token.definition}.svg`);
         await writeFile(fileXMLPath, fileXMLContent);
         // Add Definitions
@@ -115,7 +153,7 @@ async function buildFileIcons (
             fileIcons[item][definition] = iconTokenName;
           });
         });
-        resolve(iconTokenName);
+        resolve(fileSVG);
       });
     })
   }, Promise.resolve());
@@ -129,12 +167,14 @@ function merge (...items) {
       .keys(current)
       .filter((name) => {
         const value = current[name];
-        return previous[name] &&
-          typeof value == 'object' && 
-          value.constructor == Object;
+        return previous[name] && typeof value == 'object';
       })
       .forEach((name: string) => {
-        current[name] = merge(previous[name], current[name]);
+        if (Array.isArray(current[name])) {
+          current[name] = [...previous[name], ...current[name]];
+        } else {
+          current[name] = merge(previous[name], current[name]);
+        }
       });
     return Object.assign(previous, current);
   }, master);
